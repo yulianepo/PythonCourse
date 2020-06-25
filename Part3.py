39cab83fffd3142f91d365b20d40c027ead10f73
ynonp <ynonperek@gmail.com>
2020-06-25 18:48:56 +0300

    lesson19 comments

--- Part3.py
+++ Part3.py
@@ -10,6 +10,14 @@ with open('host.txt', 'r', encoding='UTF-8') as f:
         key, value = line.split('=')
         db[key] = value
 print(db)
+# So we actually don't need "i" here:
+for hostname in sys.argv[1:]
+    try:
+        print(f"the IP of {hostname} is: {db[hostname]}")
+    except KeyError:
+        print(f"for {hostname} no such IP")
+
+
 for i in range(1, len(sys.argv)):
     try:
         print(f"the IP of {sys.argv[i]} is: {db[sys.argv[i]]}")
